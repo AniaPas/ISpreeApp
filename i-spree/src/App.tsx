70bde0695d4caa97e0c897921fd37331a78cfeec
Ania <an.pasieczna@gmail.com>
2023-03-15 21:54:18 +0100

    card component added

--- i-spree/src/App.tsx
+++ i-spree/src/App.tsx
@@ -1,27 +1,11 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 import { GlobalStore } from "./Store/GlobalStore";
+import { All } from "./view/All/All";
 
 function App() {
   return (
     <GlobalStore>
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <All />
     </GlobalStore>
   );
 }
--- i-spree/src/view/All/All.tsx
+++ i-spree/src/view/All/All.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useEffect, useContext, FC } from "react";
+import { getBaskets } from "../../services/services";
+import { GlobalState } from "../../Store/GlobalStore";
+import { CardComponent } from "../../components/CardComponent/CardComponent";
+import { Grid } from "@mui/material";
 
-const All = () => {
-  return <div>All</div>;
+export const All: FC = () => {
+  const global = useContext(GlobalState);
+  const getAllBaskets = async () => {
+    try {
+      const baskets = await getBaskets();
+      return await global.globalGetBaskets(baskets.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getBaskets();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log(global.globalBaskets);
+  return (
+    <>
+      {" "}
+      <Grid container spacing={3}>
+        <CardComponent />
+      </Grid>
+    </>
+  );
 };
-
-export default All;
