3e2f918649ed9fa61e48858a50cea8e669582e14
Ania <an.pasieczna@gmail.com>
2023-03-17 13:07:51 +0100

    header and nav added

--- i-spree/src/App.tsx
+++ i-spree/src/App.tsx
@@ -1,11 +1,15 @@
 import style from "./App.module.scss";
 import { GlobalStore } from "./Store/GlobalStore";
 import { All } from "./view/All/All";
+import { Nav } from "./components/Nav/Nav";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <GlobalStore>
       <div className={style.app}>
+        <Header />
+        <Nav />
         <All />
       </div>
     </GlobalStore>
