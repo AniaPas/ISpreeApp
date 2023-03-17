0cb7b0dd050f6bb158150e5f4091381910bc4e8e
Ania <an.pasieczna@gmail.com>
2023-03-17 13:16:29 +0100

    Router Added

--- i-spree/src/App.tsx
+++ i-spree/src/App.tsx
@@ -1,8 +1,12 @@
 import style from "./App.module.scss";
 import { GlobalStore } from "./Store/GlobalStore";
 import { All } from "./view/All/All";
+import { Add } from "./view/Add/Add";
 import { Nav } from "./components/Nav/Nav";
 import { Header } from "./components/Header/Header";
+
+// router
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -10,7 +14,10 @@ function App() {
       <div className={style.app}>
         <Header />
         <Nav />
-        <All />
+        <Routes>
+          <Route path='/' element={<All />} />
+          <Route path='/add' element={<Add />} />
+        </Routes>
       </div>
     </GlobalStore>
   );
